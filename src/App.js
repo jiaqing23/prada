d59a4586ffc33e6d24c5793a6df35def36067f12
Jia Qing <jiaqing2303@gmail.com>
2021-11-16 13:20:55 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import FooterBar from "./components/Footer";
+import TestPage from "./pages/TestPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={<LandingPage child={<TestPage title={"Home Page"} />} />}
+        />
+        <Route
+          path="/overview"
+          element={<LandingPage child={<TestPage title={"Overview"} />} />}
+        />
+        <Route
+          path="/company"
+          element={<LandingPage child={<TestPage title={"Company"} />} />}
+        />
+        <Route
+          path="/contact"
+          element={<LandingPage child={<TestPage title={"Contact"} />} />}
+        />
+        <Route
+          path="/login"
+          element={<LandingPage child={<TestPage title={"Login"} />} />}
+        />
+      </Routes>
+      <FooterBar />
+    </Router>
   );
 }
 
